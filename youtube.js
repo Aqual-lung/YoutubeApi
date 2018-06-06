238d12eefd73cd10bb67a29332ec5e9a0c3e8c63
Aqual-lung <38408628+Aqual-lung@users.noreply.github.com>
2018-06-06 13:07:03 -0500

    this is the javascript code we are working with

---
+++ youtube.js
@@ -0,0 +1,26 @@
+# YoutubeApi
+Youtube search engine
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Simple Youtube search engine</title>
+</head>
+<body>
+<h1> A simple youtube search engine</h1>
+<input type="text" id = "key" name= "key">
+<button id="switch">enter</button>
+<div>
+  <ul>
+    <li id="D1"></li>
+    <li id="D2"></li>
+    <li id="D3"></li>
+    <li id="D4"></li>
+    <li id="D5"></li>
+  </ul>
+</div>
+     
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="youtube.js"></script>
+</body>
+</html>
